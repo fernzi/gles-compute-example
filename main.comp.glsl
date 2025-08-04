6d91b100a38ba1af9c96dec2d12de95dfcf005b0
Fern Zapata <fern@zapata.cc>
2025-08-04 05:14:31 -0600

    Add a shader that actually computes

--- main.comp.glsl
+++ main.comp.glsl
@@ -2,4 +2,33 @@
 
 layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 
-void main() {}
+layout(binding = 0) buffer SSBO
+{
+  uint values[];
+};
+
+layout(location = 0) uniform uint elements;
+
+uint fibonacci(uint n)
+{
+  if (n <= 1u) {
+    return n;
+  }
+  uint curr = 1u;
+  uint prev = 1u;
+  for (uint i = 2u; i < n; ++i) {
+    uint temp = curr;
+    curr += prev;
+    prev = temp;
+  }
+  return curr;
+}
+
+void main()
+{
+  uint index = gl_GlobalInvocationID.x;
+  if (index >= elements) {
+    return;
+  }
+  values[index] = fibonacci(values[index]);
+}
